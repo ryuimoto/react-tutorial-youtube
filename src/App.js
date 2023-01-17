9678b59e77b8307096f80a63789ea46336f774fb
ryuimoto <narrow617@gmail.com>
2023-01-17 15:08:38 +0900

    done

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const handleAddTodo = () => {
     // タスクを追加する。
     const name = todoNameRef.current.value;
+    if(name === "") return;
     setTodos((prevTodos) => {
       return [...prevTodos,{id: uuidv4(),name:name,compleated:false}];
     });
@@ -26,12 +27,17 @@ function App() {
     setTodos(newTodos);
   };
 
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.compleated);
+    setTodos(newTodos);
+  }
+
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input type="text" ref={todoNameRef}/>
       <button onClick={handleAddTodo}>タスクを追加</button>
-      <button type="">完了したタスクの削除</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
       <div>残りのタスク:{todos.filter((todo) => !todo.compleated).length}</div>
     </>
   );
