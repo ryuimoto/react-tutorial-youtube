a5f94c20cad0d7950c359c4309f14c494c9d167e
ryuimoto <narrow617@gmail.com>
2023-01-17 13:26:13 +0900

    add Todo.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos,setTodos] = useState(["Todo1","Todo2"]);
+
   return (
-    <div>
-      <TodoList />
-    </div>
+    <>
+      <TodoList todos={todos} />
+      <input type="text"/>
+      <button type="">タスクを追加</button>
+      <button type="">完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
   );
 }
 
